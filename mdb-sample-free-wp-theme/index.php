6a682bb931f034da14d59990ea4e6eccde6ceb6e
rodri6666 <morfeo_che@hotmail.com>
2023-12-15 19:39:55 -0400

    subiendo angular + mdb

---
+++ mdb-sample-free-wp-theme/index.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+    <div class="container">
+        <div class="text-center">
+            <img src="https://mdbootstrap.com/img/logo/mdb-transparent-big.png" alt="Logo" width="500px" class="">
+
+            <h2>MDB Sample Theme</h2>
+        </div>
+    </div>
+
+<?php
+
+get_footer();
